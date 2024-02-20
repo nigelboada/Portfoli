4f38dfafbe27383a519c6b7505aaf1590eb325b9
nigelboada <nboadag@gmail.com>
2024-02-20 15:47:15 +0100

    update new branch s2

---
+++ src/HelloWorldServer.java
@@ -0,0 +1,38 @@
+import java.io.*;
+import java.net.ServerSocket;
+import java.net.Socket;
+
+public class HelloWorldServer {
+    public static void main(String[] args) {
+        int port = 1234;
+        try {
+            ServerSocket ss = new ServerSocket(port);
+            System.out.println("Server: Esperant connexions...");
+            Socket s = ss.accept();
+            System.out.println("Server: Connexió establerta");
+
+            InputStream is = s.getInputStream();
+            DataInputStream dis = new DataInputStream(is);
+
+            while (true) {
+                try {
+                    String message = dis.readUTF(), newMessage;
+                    newMessage = message.toUpperCase();
+                    OutputStream os = s.getOutputStream();
+                    DataOutputStream dos = new DataOutputStream(os);
+                    dos.writeUTF(newMessage);
+
+                } catch (IOException e) {
+                    System.out.println("Server: Connexió tancada pel client.");
+                    break;  // Salir del bucle si la conexión se cierra
+                }
+            }
+
+            dis.close();
+            s.close();
+            ss.close();
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+}
