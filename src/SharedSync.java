d19ef64aeaac6c8d4d1b01c025d61d2b325de5fd
nigelboada <nboadag@gmail.com>
2024-03-11 16:09:36 +0100

    update new branch

--- src/SharedSync.java
+++ src/SharedSync.java
@@ -4,11 +4,14 @@ public class SharedSync{
     public static void main(String[] args) throws InterruptedException {
         Thread t1 = new Thread(new Adder());
         Thread t2 = new Thread(new Adder());
+        Thread t3 = new Thread(new Decrementer());
 
         t1.start();
         t2.start();
+        t3.start();
         t1.join();
         t2.join();
+        t3.join();
         System.out.println("x = "+ x);
 
     }
@@ -22,6 +25,16 @@ public class SharedSync{
             }
             System.out.println("FINAL");
 
+        }
+    }
+    public static class Decrementer implements Runnable {
+        public void run() {
+            for (int i = 0; i < 500000; i++) {
+                synchronized (locker) {
+                    x = x - 1;
+                }
+            }
+            System.out.println("Decrementer FINAL");
         }
     }
 }
